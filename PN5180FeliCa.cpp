a022e8e55487fd946c3da9a2e0ff8a3305580b30
Trappmann <Andreas.Trappmann@swisslife.de>
2020-03-17 16:53:39 +0100

    Fixed bad call to PN5180::readData

--- PN5180FeliCa.cpp
+++ PN5180FeliCa.cpp
@@ -23,7 +23,7 @@
 #include <PN5180.h>
 #include "Debug.h"
 
-PN5180FeliCa::PN5180FeliCa(uint8_t SSpin, uint8_t BUSYpin, uint8_t RSTpin) 
+PN5180FeliCa::PN5180FeliCa(uint8_t SSpin, uint8_t BUSYpin, uint8_t RSTpin)
               : PN5180(SSpin, BUSYpin, RSTpin) {
 }
 
@@ -58,12 +58,12 @@ uint8_t PN5180FeliCa::pol_req(uint8_t *buffer) {
 	uint8_t cmd[6];
 	uint8_t uidLength = 0;
 	// Load FeliCa 424 protocol
-	if (!loadRFConfig(0x09, 0x89)) 
+	if (!loadRFConfig(0x09, 0x89))
 	  return 0;
 	// OFF Crypto
 	if (!writeRegisterWithAndMask(SYSTEM_CONFIG, 0xFFFFFFBF))
 	  return 0;
-    
+
 	//send FeliCa request (every packet starts with length)
     cmd[0] = 0x06;             //total length
 	cmd[1] = 0x00;             //POL_REQ command
@@ -71,16 +71,21 @@ uint8_t PN5180FeliCa::pol_req(uint8_t *buffer) {
 	cmd[3] = 0xFF;             // any target
 	cmd[4] = 0x01;             // System Code request
 	cmd[5] = 0x00;             // 1 timeslot only
-    
+
 	if (!sendData(cmd, 6, 0x00))
 	  return 0;
     //wait a little to avoid bug with some cards
     delay(50);
-   
+
     //response packet should be 0x14 (20 bytes total length), 0x01 Response Code, 8 IDm bytes, 8 PMm bytes, 2 Request Data bytes
     //READ 20 bytes reply
-	if (!readData(20, buffer)) 
+  uint8_t *internalBuffer = readData(20);
+	if (!internalBuffer)
 	  return 0;
+ for (int i=0; i<20; i++) {
+    buffer[i] = internalBuffer[i];
+ }
+ 
     //check Response Code
     if ( buffer[1] != 0x01 ){
         uidLength = 0;
@@ -91,23 +96,23 @@ uint8_t PN5180FeliCa::pol_req(uint8_t *buffer) {
 }
 
 uint8_t PN5180FeliCa::readCardSerial(uint8_t *buffer) {
-  
+
     uint8_t response[20];
 	uint8_t uidLength;
-    
+
     for (int i = 0; i < 20; i++)
         response[i] = 0;
-    
+
     uidLength = pol_req(response);
-    
+
     // no reply
 	if (uidLength == 0)
 	  return 0;
-  
-    for (int i = 0; i < uidLength; i++) 
+
+    for (int i = 0; i < uidLength; i++)
         buffer[i] = response[i+2];
-    
-	return uidLength;  
+
+	return uidLength;
 }
 
 bool PN5180FeliCa::isCardPresent() {
